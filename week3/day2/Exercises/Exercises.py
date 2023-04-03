d14c8dc1db9bb4046f45d02509bf469e98d74d33
Menbenny <menacbinyamin@gmail.com>
2023-04-03 21:24:27 +0300

    day2_exercises

---
+++ week3/day2/Exercises/Exercises.py
@@ -0,0 +1,17 @@
+class Door:
+    def __init__(self, is_opened=False):
+        self.is_opened = is_opened
+
+    def open_door(self):
+        self.is_opened = True
+
+    def close_door(self):
+        self.is_opened = False
+
+class BlockedDoor(Door):
+    def open_door(self):
+        raise ValueError("Blocked door cannot be opened.")
+    
+    def close_door(self):
+        raise ValueError("Blocked door cannot be closed.")
+        
