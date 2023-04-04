167036e77429aa0c4cfc1383e6034ee85bb78629
Menbenny <menacbinyamin@gmail.com>
2023-04-04 17:48:50 +0300

    exeXP

--- week3/day3/Exercises/exerises.py
+++ week3/day3/Exercises/exerises.py
@@ -45,13 +45,79 @@
 
 # COMPARISON
 
-class Car:
-    def __init__(self, speed):
-        self.speed = speed
+# All operators have default dunder method - 
 
-    def __gt__(self, other_car):
-        return self.speed > other_car.speed 
+# class Car:
+#     def __init__(self, speed):
+#         self.speed = speed
+# # gt- greater than 
+#     def __gt__(self, other_car):
+#         return self.speed > other_car.speed 
+# # eq - equal
+#     def __eq__(self, other_car):
+#         return self.speed ==other_car.speed
+# # +
+#     def __add__(self, other_car):
+#         return self.speed + other_car.speed
+    
+#     def __iadd__(self, other_car):
+#         self.speed += other_car.speed 
+#         return self 
 
-car1 = Car[200]
-car2 = Car[300]
+# car1 = Car[200]
+# car2 = Car[300]
+        
+# car1 += car2
+# car1 = car2        
+
+# ##########################################
+
+# EXERCISE 1 : Built-In Functions 
+
+def Document():
+    """
+    The code prompts the user to enter an input. Input is
+    then convereted to an integer through the int function,
+    the absolute value of the integer is what is printed out.
+    """
+
+num_str = input("Enter a number: ")
+
+num_int = int(num_str)
+
+num_abs = abs(num_int)
+
+######################################
+
+# EXERCISE 2: Currencies 
+
+class Currency:
+    def __init__(self, currency, amount):
+        self.currency = currency 
+        self.amount = amount 
+          
+    def __str__(self):
+        return f"{self.amount} {self.currency}"
+    
+    def __init__(self):
+        return self.amount
+    
+    def __repr__(self):
+        return f"{self.amount} {self.currency}"
+    
+    def __add__(self, other):
+        if isinstance(other, Currency):
+            if self.currency !=other.currency:
+                raise TypeError(f"Cannot add between Currency type {self.currency} and {other.currency}")
+            return Currency(self.currency, self.amount + other.amount)
+        else:
+            return Currency(self.currency, self.amount + other)
+        
+    def __iadd__(self, other):
+        if isinstance(other, Currency):
+            if self.currency != other.currency:
+                raise TypeError(f"Cannot add between Currency {self.currency} and {other.currency}")
+            
+
+        
         
