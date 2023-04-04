580c3cf3d1d7d5933eb8d52848ea358ea1a8bf7a
Menbenny <menacbinyamin@gmail.com>
2023-04-04 20:23:57 +0300

    exeXP+

--- week3/day3/ExercisesXP+/exercise_one.py
+++ week3/day3/ExercisesXP+/exercise_one.py
@@ -1,13 +1,32 @@
-import func  
+# import func  
 
-########################################################
+# ########################################################
 
-# EXERCISE 2: Random Module
+# # EXERCISE 2: Random Module
 
-def func100(num):
-    num = 0 > 101
+# def func100(num):
+#     num = 0 > 101
 
-    def __int__(num):
-        while True: 
-            print("Number accepted.")
+#     def __int__(num):
+#         while True: 
+#             return "Number accepted"
             
+# ########################################################
+
+# # EXERCISE 3: String Module
+
+# # random_str = 'a' 
+
+# import random
+# import string 
+
+# my_string = '' .join(random.choices(string.ascii_lowercase + string.ascii_uppercase, k=5))
+
+# ########################################################
+
+# # EXERCISE 4: Current Date
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
