3030d8dff47e80fb3e7496e3ca1421c6985f7408
Menbenny <menacbinyamin@gmail.com>
2023-04-04 20:24:11 +0300

    exeXP+

--- week3/day3/ExercisesXP+/exercise_one.py
+++ week3/day3/ExercisesXP+/exercise_one.py
@@ -1,32 +1,37 @@
-# import func  
+import func  
 
-# ########################################################
+########################################################
 
-# # EXERCISE 2: Random Module
+# EXERCISE 2: Random Module
 
-# def func100(num):
-#     num = 0 > 101
+def func100(num):
+    num = 0 > 101
 
-#     def __int__(num):
-#         while True: 
-#             return "Number accepted"
+    def __int__(num):
+        while True: 
+            return "Number accepted"
             
-# ########################################################
+########################################################
 
-# # EXERCISE 3: String Module
+# EXERCISE 3: String Module
 
-# # random_str = 'a' 
+# random_str = 'a' 
 
-# import random
-# import string 
+import random
+import string 
 
-# my_string = '' .join(random.choices(string.ascii_lowercase + string.ascii_uppercase, k=5))
+my_string = '' .join(random.choices(string.ascii_lowercase + string.ascii_uppercase, k=5))
 
-# ########################################################
+########################################################
 
-# # EXERCISE 4: Current Date
+# EXERCISE 4: Current Date
 
 import datetime
 
 x = datetime.datetime.now()
 print(x)
+
+########################################################
+
+# EXERCISE 5: Amount Of Time Left Until January 1st
+
