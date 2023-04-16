7d7b6426cacb0afa6a3d2b1ee398d6f2382fefdc
Menbenny <menacbinyamin@gmail.com>
2023-04-16 16:07:56 +0300

    dailyChallange

--- week4/daily_challange/dailychallange.sql
+++ week4/daily_challange/dailychallange.sql
@@ -1,7 +1,10 @@
 			-- Daily Challange -- 
 
-insert into students (last_name, first_name, birth_date)
-values
-('', '', '')
-('', '', '')
+-- insert into students (last_name, first_name, birth_date)
+-- values
+-- (NULL, NULL, NULL)
+
+delete from students where id = 8
+
+
 
