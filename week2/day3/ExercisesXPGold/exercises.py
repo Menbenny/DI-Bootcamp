f35d168de8645fbdf4abec245a21c7e2ca64525f
Menbenny <menacbinyamin@gmail.com>
2023-04-25 03:23:22 +0300

    exercisesXP_GOLD

---
+++ week2/day3/ExercisesXPGold/exercises.py
@@ -0,0 +1,39 @@
+#                   Exercises XP GOLD - week2 day2
+
+# list1 = ['ben', 'jay']
+# list2 = ['Mariam', 'Ofri']
+
+# for x in list2:
+#     list1.append(x)
+#     print(list1)
+
+# range = int(1500 <= 2500)
+
+# while range // 5 == True or range // 7 == True:
+#     print(range)
+
+# for i in range(1500, 2501):
+#     if i % 5 == 0 or i % 7 == 0:
+#         print(i)
+d = ('%y/%m/%d')
+birthdays = {
+    'Jay': '1996/05/18)',
+    'Mia': '1977/02/19)',
+    'Yon': '1992/12/23)',
+    'Ian': '1998/04/17)',
+    'Paul': '1990/09/27)'
+}
+
+keys = list(birthdays.keys())
+values = list(birthdays.values())
+
+print(keys)
+
+# print("You can look up the birthdays of the people in the list!")
+# i = input("Please enter a name: ")
+
+# for keys in birthdays:
+#     if i == keys[]:
+#       
+#         print(i + '')
+
