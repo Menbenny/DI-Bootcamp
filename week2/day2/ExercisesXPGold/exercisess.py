53ca2a68e99ea9fc6f59a45643493f7265389d28
Menbenny <menacbinyamin@gmail.com>
2023-05-08 14:40:25 +0300

    week2XP_GOLD

--- week2/day2/ExercisesXPGold/exercisess.py
+++ week2/day2/ExercisesXPGold/exercisess.py
@@ -7,6 +7,9 @@
 
 # print(lists)
 
+
+
+
 #  exercise 2
 
 # numbers = range(1500, 2501) # In Range NB end number/index always one plus
@@ -15,7 +18,11 @@
 #     if num % 5 == 0 and num & 7 == 0:
 #         print(num)
 
+
+
+
 # exercise 3
+
 # names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
 
 # user_name = input("Please enter your name: ")
@@ -26,23 +33,19 @@
 # else:
 #     print("name doesn't match the list")
     
+
+
+
 # Exercise 4
-user_input = input('Enter 3 different numbers: ')
-numbers = user_input.split()
+
+# user_input = input('Enter 3 different numbers: ')
+# numbers = user_input.split()
  
-for num in numbers:
-      if num == max(user_input):
-            print(num)
+# for num in numbers:
+#       if num == max(user_input):
+#             print(num)
 
-print(num)
-
-
-
-
-
-
-
-
+# print(num)
 
 
 # def user_name():
@@ -50,5 +53,33 @@ print(num)
 #     if  user_name in names:
         
 # print(names.index(user_name))
+
+
+
+
+# Exercise 5
+
+# Create a string of all the letters in the alphabet
+# Loop over each letter and print a message that contains the letter and whether its a vowel or a consonant.
+
+alphabet = ("abcdefghijklmnopqrstuvwxyz")
+vowels=0
+consonant=0
+
+
+
+for letter in alphabet:
+    if letter in 'aeiou':
+        print(f"letter {letter} is a vowel")
+    else:
+        print(f"letter {letter} is a consonant")
+          
+
+
+
+
+
+
+
 
 
