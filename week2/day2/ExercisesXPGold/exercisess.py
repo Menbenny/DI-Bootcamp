f8cdbbf489550bc40e5da197bef7e008e6162a64
Menbenny <menacbinyamin@gmail.com>
2023-05-08 14:06:19 +0300

    week2-Gold

--- week2/day2/ExercisesXPGold/exercisess.py
+++ week2/day2/ExercisesXPGold/exercisess.py
@@ -1,21 +1,5 @@
-# list1 = [5, 10, 15, 20, 25, 50, 20]
-# list1[3] = 200
-# print(list1[3])
-
-# BEST PRACTICE
-
-# list1 = [5, 10, 15, 20, 25, 50, 20]
-# position = list1.index(20)
-# list1[position] = 200
-
-
-
-# a_tuple = (10, 20, 30, 40)
-# a_tuple = 10, 20, 30, 40
-# a_tuple = "a", "b", "c", "d", "e"
-
-
 # exercise 1 gold XP
+
 # list_a = [1, 2, 3, 4, 5]
 # list_b = ["Me", "You", "Them", "Us", "They"]
 
@@ -24,6 +8,7 @@
 # print(lists)
 
 #  exercise 2
+
 # numbers = range(1500, 2501) # In Range NB end number/index always one plus
 
 # for num in numbers:
@@ -31,19 +16,25 @@
 #         print(num)
 
 # exercise 3
-names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+# names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
 
-user_name = input("Please enter your name: ")
+# user_name = input("Please enter your name: ")
 
-if user_name in names:
-        index = names.index(user_name)
-        print(f'{index}')
-else:
-    print("name doesn't match the list")
+# if user_name in names:
+#         index = names.index(user_name)
+#         print(f'{index}')
+# else:
+#     print("name doesn't match the list")
     
+# Exercise 4
+user_input = input('Enter 3 different numbers: ')
+numbers = user_input.split()
+ 
+for num in numbers:
+      if num == max(user_input):
+            print(num)
 
-
-
+print(num)
 
 
 
