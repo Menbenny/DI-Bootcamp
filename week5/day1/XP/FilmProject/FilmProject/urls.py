c793b156481bab577772a00575d1486d77ceacd0
Menbenny <menacbinyamin@gmail.com>
2023-06-05 22:30:45 +0300

    w5d1

--- week5/day1/XP/FilmProject/FilmProject/urls.py
+++ week5/day1/XP/FilmProject/FilmProject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from FilmProject import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('FilmProject.urls')),
+
 ]
--- week5/day1/XP/FilmProject/films/models.py
+++ week5/day1/XP/FilmProject/films/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Country(models.Model):
@@ -14,4 +15,9 @@ class Director(models.Model):
 
 class Film(models.Model):
     tittle = models.CharField(max_length=100)
-    release_date = models.DateField()
+    release_date = models.DateField(auto_now_add=True)
+    created_in_country = models.ForeignKey(Country, 
+                                           on_delete=models.CASCADE,
+                                           related_name='films_created')
+    category = models.ManyToManyField(Category)
+    director = models.ManyToManyField(Director)
