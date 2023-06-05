36a138b18a8828b56d0b9abecc4404944562c325
Menbenny <menacbinyamin@gmail.com>
2023-06-05 22:15:18 +0300

    w5d1XP

--- week5/day1/XP/FilmProject/films/models.py
+++ week5/day1/XP/FilmProject/films/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Director(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+class Film(models.Model):
+    tittle = models.CharField(max_length=100)
+    release_date = models.DateField()
